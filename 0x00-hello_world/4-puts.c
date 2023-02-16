2ebb90e20ad2e548ba36bbd80fd48c41bf32aca9
MosesOdinaka <iriele.moses@gmail.com>
2023-02-16 13:58:08 -0800

    C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * printing a line with puts function:
+ *main - Printing a line with puts function
+ *Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
