bf7ce6e062b0a6d3f62a8263eebd899a565788f7
MosesOdinaka <iriele.moses@gmail.com>
2023-02-16 16:10:30 -0800

    C program that prints a lint

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints a line to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
+		
+}
