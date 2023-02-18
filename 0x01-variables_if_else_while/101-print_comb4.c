258762811d8ef88ddad30bdf4d2e4e2fac834eb5
MosesOdinaka <iriele.moses@gmail.com>
2023-02-17 17:19:32 -0800

    prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The entry point.
+ * Return: 0 Always (true).
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+	for (b = a + 1; b <= '9'; b++)
+	{
+	for (c = b + 1; c <= '9'; c++)
+	{
+	if ((b != a) != c)
+	{
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	if (a == '7' && b == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
