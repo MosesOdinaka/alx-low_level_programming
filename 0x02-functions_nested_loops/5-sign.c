f7f76a5c17500c8c073fc8ab8b6f560d6a595eef
MosesOdinaka <iriele.moses@gmail.com>
2023-02-21 10:15:37 -0800

    function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - The sign of a number.
+ * @n: Number to be check.
+ * Return: 1 for positive num, -1 negative num or zero for for anything else.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
