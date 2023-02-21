0240541ebc45bdee0301865b2e13e2a30d170ff5
MosesOdinaka <iriele.moses@gmail.com>
2023-02-20 20:29:31 -0800

    prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float %zu byte(s)\n", sizeof(float));
-	printf("\n");
+	fprintf(stdout, "Size of a char: %zu byte(s)\n", sizeof(char));
+	fprintf(stdout, "Size of an int: %zu byte(s)\n", sizeof(int));
+	fprintf(stdout, "Size of a long int: %zu byte(s)\n", sizeof(long int));
+	fprintf(stdout, "Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	fprintf(stdout, "Size of a float %zu byte(s)\n", sizeof(float));
+	fprintf(stdout, "\n");
+	fprintf(stderr, "There is only one character\n");
 	return (0);
 }
