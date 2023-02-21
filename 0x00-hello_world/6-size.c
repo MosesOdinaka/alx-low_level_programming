850143b51af6fa7bdea3061910952c09b41f3a11
MosesOdinaka <iriele.moses@gmail.com>
2023-02-20 20:45:53 -0800

    prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main -
- *Return: 0.
+ * main - Entry point
+ *Return: 0
  */
 
 int main(void)
 {
-	fprintf(stdout, "Size of a char: %zu byte(s)\n", sizeof(char));
-	fprintf(stdout, "Size of an int: %zu byte(s)\n", sizeof(int));
-	fprintf(stdout, "Size of a long int: %zu byte(s)\n", sizeof(long int));
-	fprintf(stdout, "Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	fprintf(stdout, "Size of a float %zu byte(s)\n", sizeof(float));
-	fprintf(stdout, "\n");
-	fprintf(stderr, "[Anything]\n");
-	return (0);
+fprintf(stdout, "Size of a char: %zu byte(s)\n", sizeof(char));
+fprintf(stdout, "Size of an int: %zu byte(s)\n", sizeof(int));
+fprintf(stdout, "Size of a long int: %zu byte(s)\n", sizeof(long int));
+fprintf(stdout, "Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+fprintf(stdout, "Size of a float %zu byte(s)\n", sizeof(float));
+fprintf(stdout, "\n");
+fprintf(stderr, "[Anything]\n");
+return (0);
 }
