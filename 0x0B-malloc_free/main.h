67abead2ca5bf72f03eccc4aeb2b3d10b9d2dcf8
MosesOdinaka <iriele.moses@gmail.com>
2023-03-13 18:52:57 -0700

    The header file containing the prototypes.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -6,3 +9,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+
+#endif
