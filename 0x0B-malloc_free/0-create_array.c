822861e192c8fc4483fa7c22bf5e0b64460dce27
MosesOdinaka <iriele.moses@gmail.com>
2023-03-13 17:47:27 -0700

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars, and initializes it with a
+ * specific character
+ * @size: size of array
+ * @c: character to initialize with
+ *
+ * Return: pointerr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size <= 0)
+		return (0);
+
+	s = malloc(sizeof(char) * size);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		*(s + i) = c;
+
+	*(s + i) = '\0';
+
+	return (s);
+}
