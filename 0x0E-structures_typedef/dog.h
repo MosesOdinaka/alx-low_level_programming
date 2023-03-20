3c22cbee3bd4b2f5cbc87c9381782ad4bc16edc6
MosesOdinaka <iriele.moses@gmail.com>
2023-03-20 12:33:22 -0700

    Defining a new type struct dog with the following elements: name, type = char * age, type = float and owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - description for a pet dog.
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner
+ *
+ * Description: long description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
