4cfb04b67a819ff5364028d4a764a116316fdd11
MosesOdinaka <iriele.moses@gmail.com>
2023-03-30 06:37:10 -0700

    Header file for function prototype.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-_putchar(char c);
+int _putchar(char c);
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
