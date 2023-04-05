af2deefbd8e15d4a4ca2fa4b2b92c716815944ac
MosesOdinaka <iriele.moses@gmail.com>
2023-04-04 17:26:16 -0700

    A function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
+}
