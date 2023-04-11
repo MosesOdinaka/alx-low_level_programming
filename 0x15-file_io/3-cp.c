106e5f14e0026d903b24712fdd791af84f96486a
Moses Iriele <iriele.moes@gmail.com>
2023-04-12 00:25:36 +0100

    A program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - program that copies the content of a file to another file
+* @argc: num argument
+* @argv: string argument
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to;
+int numb1 = 1024, numb2 = 0;
+char buff[1024];
+
+if (argc != 3)
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+if (fd_to == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(fd_from), exit(99);
+}
+while (numb1 == 1024)
+{
+	numb1 = read(fd_from, buff, 1024);
+	if (numb1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	numb2 = write(fd_to, buff, numb1);
+	if (numb2 < numb1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
+
+if (close(fd_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+if (close(fd_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+return (0);
+}
