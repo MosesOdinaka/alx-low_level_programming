5801ff06de5da78a61ba6da94e7de2ead577e0fa
MosesOdinaka <iriele.moses@gmail.com>
2023-04-14 21:39:59 -0700

    Prints the ABI version of an elf header.

--- 0x15-file_io/100-elf_header.c/print_abi.c
+++ 0x15-file_io/100-elf_header.c/print_abi.c
@@ -14,37 +14,3 @@ void print_abi(unsigned char *me_ident)
 	printf("  ABI Version:                       %d\n",
 	       me_ident[EI_ABIVERSION]);
 }
-
-/**
- * print_type - Prints the type of an elf header.
- * @me_type: The elf type.
- * @me_ident: A pointer to an array containing the elf class.
- */
-void print_type(unsigned int me_type, unsigned char *me_ident)
-{
-	if (me_ident[EI_DATA] == elfDATA2MSB)
-		me_type >>= 8;
-
-	printf("  Type:                              ");
-
-	switch (me_type)
-	{
-	case ET_NONE:
-		printf("NONE (None)\n");
-		break;
-	case ET_REL:
-		printf("REL (Relocatable file)\n");
-		break;
-	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
-		printf("DYN (Shared object file)\n");
-		break;
-	case ET_CORE:
-		printf("CORE (Core file)\n");
-		break;
-	default:
-		printf("<unknown: %x>\n", me_type);
-	}
-}
