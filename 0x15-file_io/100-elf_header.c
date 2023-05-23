5b626913176e93ccaa0cd7a271e6e74c2fcb828b
Moses Iriele <iriele.moes@gmail.com>
2023-05-23 01:58:49 +0100

    correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,59 +4,69 @@
 #include <fcntl.h>
 #include <elf.h>
 
-int main(int argc, char **argv) {
-    int fd, i;
-    ssize_t bytes_read;
-    Elf64_Ehdr header;
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        exit(98);
-    }
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Error: Cannot read file %s\n", argv[1]);
-        exit(98);
-    }
-    bytes_read = read(fd, &header, sizeof(header));
-    if (bytes_read != sizeof(header)) {
-        fprintf(stderr, "Error: Cannot read file %s\n", argv[1]);
-        close(fd);
-        exit(98);
-    }
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "%s: Error: Not an ELF file - it has the wrong magic bytes at the start\n", argv[0]);
-        close(fd);
-        exit(98);
-    }
-    printf("ELF Header:\n Magic: ");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf("%02x ", header.e_ident[i]);
-    printf("\n Class: ");
-    switch (header.e_ident[EI_CLASS]) {
-        case ELFCLASSNONE:
-            printf("none\n");
-            break;
-        case ELFCLASS32:
-            printf("ELF32\n");
-            break;
-        case ELFCLASS64:
-            printf("ELF64\n");
-            break;
-        default:
-            printf("<unknown: %x>\n", header.e_ident[EI_CLASS]);
-            break;
-    }
-    printf(" Data: ");
-    switch (header.e_ident[EI_DATA]) {
-        case ELFDATANONE:
-            printf("none\n");
-            break;
-        case ELFDATA2LSB:
-            printf("2's complement, little endian\n");
-            break;
-        case ELFDATA2MSB:
-            printf("2's complement, big endian\n");
-	    break;
-    }
-    return (0);
+int main(int argc, char **argv)
+{
+	int fd, i;
+	ssize_t bytes_read;
+	Elf64_Ehdr header;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error: Cannot read file %s\n", argv[1]);
+		exit(98);
+	}
+	bytes_read = read(fd, &header, sizeof(header));
+	if (bytes_read != sizeof(header))
+	{
+		fprintf(stderr, "Error: Cannot read file %s\n", argv[1]);
+		close(fd);
+		exit(98);
+	}
+	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1]
+			!= ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 ||
+			header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "%s: Error: Not an ELF file - it has the
+				wrong magic bytes at the start\n", argv[0]);
+		close(fd);
+		exit(98);
+	}
+	printf("ELF Header:\n Magic: ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header.e_ident[i]);
+	printf("\n Class: ");
+	switch (header.e_ident[EI_CLASS])
+	{
+		case ELFCLASSNONE:
+			printf("none\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header.e_ident[EI_CLASS]);
+			break;
+	}
+	printf(" Data: ");
+	switch (header.e_ident[EI_DATA])
+	{
+		case ELFDATANONE:
+			printf("none\n");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
+			break;
+	}
+	return (0);
 }
