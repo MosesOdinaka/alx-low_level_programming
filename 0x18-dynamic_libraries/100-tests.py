898fe20cac7285f8105c0737e687de707d5f02a9
MosesOdinaka <iriele.moses@gmail.com>
2023-06-21 04:23:34 -0700

    python program calling dynamic library.

---
+++ 0x18-dynamic_libraries/100-tests.py
@@ -0,0 +1,9 @@
+import random
+import ctypes
+
+cops = ctypes.CDLL('./100-operations.so')
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
