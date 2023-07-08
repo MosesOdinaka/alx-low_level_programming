f9d5e700cda7a6ca6f5ae3be4cae9fc65dea6cbc
MosesOdinaka <iriele.moses@gmail.com>
2023-07-07 19:11:20 -0700

    This function takes as arguments the hash table ht you want to add or update the key/value to, the key, and the value associated with the key.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,26 +1,41 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: the key
+ * @value: the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
-int main(void)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *ht;
-	char *s;
-	unsigned long int hash;
+	unsigned long int index;
+	hash_node_t *new_node, *temp;
 
-	ht = hash_table_create(1024);
-	printf("%p\n", (void *)ht);
+	if (!ht || !key || !*key || !value)
+		return (0);
 
-	s = "cisfun";
-	hash = hash_djb2((unsigned char *)s);
-	printf("%lu\n", hash);
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
 
-	printf("%lu\n", key_index((unsigned char *)s, ht->size));
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
 
-	hash_table_set(ht, "betty", "cool");
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-	return (EXIT_SUCCESS);
+	return (1);
 }
