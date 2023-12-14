859a21ec4b92b9ea8105dfa7d8604793d8167657
MosesOdinaka <iriele.moses@gmail.com>
2023-12-14 01:43:23 +0100

    Updated the hHeader file.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+void print_array(const int *array, size_t size);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
